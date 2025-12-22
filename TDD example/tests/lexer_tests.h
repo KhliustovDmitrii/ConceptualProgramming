fd8f858160d4525214946f2ec3e33af118202489
dimon <hlustov.d@gmail.com>
2025-12-22 16:58:49 +0300

    trying tdd

---
+++ TDD example/tests/lexer_tests.h
@@ -0,0 +1,15 @@
+#ifndef _LEXER_TESTS_H_
+#define _LEXER_TESTS_H_
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include "../lexer/lexer.h"
+
+
+struct LexerTest : public testing::Test
+{
+    void SetUp(){};
+    void TearDown(){};
+};
+#endif
